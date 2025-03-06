c0c7fe291307f7ff10dee99ab423395f9db55cef
Trxyebeep <woopstombraider@gmail.com>
2025-03-06 16:27:43 -0600

    Add raw version to _platform_and_version, add support for TR3 PSX ECTS Demo levels;

--- trlevel/LevelVersion.cpp
+++ trlevel/LevelVersion.cpp
@@ -11,27 +11,28 @@ namespace trlevel
     {
         if (version == 0x34585254)
         {
-            return { .platform = Platform::PC, .version = LevelVersion::Tomb4, .remastered = true };
+            return { .platform = Platform::PC, .version = LevelVersion::Tomb4, .remastered = true, .raw_version = version };
         }
         else if (version == 0x35585254)
         {
-            return { .platform = Platform::PC, .version = LevelVersion::Tomb5, .remastered = true };
+            return { .platform = Platform::PC, .version = LevelVersion::Tomb5, .remastered = true, .raw_version = version };
         }
 
         switch (version & 0xff)
         {
         case 0x20:
-            return { .platform = (version & 0xff00) ? Platform::PSX : Platform::PC, .version = LevelVersion::Tomb1 };
+            return { .platform = (version & 0xff00) ? Platform::PSX : Platform::PC, .version = LevelVersion::Tomb1, .raw_version = version };
         case 0x2D:
-            return { .platform = Platform::PC, .version = LevelVersion::Tomb2 };
+            return { .platform = Platform::PC, .version = LevelVersion::Tomb2, .raw_version = version };
         case 0x34:
         case 0x38:
-            return { .platform = Platform::PC, .version = LevelVersion::Tomb3 }; 
+            return { .platform = Platform::PC, .version = LevelVersion::Tomb3, .raw_version = version }; 
         case 0xc8:
-            return { .platform = Platform::PSX, .version = LevelVersion::Tomb3 };
+        case 0xcb:  //TR3 PSX ECTS Demo
+            return { .platform = Platform::PSX, .version = LevelVersion::Tomb3, .raw_version = version };
         case 0x54:
-            return { .platform = Platform::PC, .version = LevelVersion::Tomb4 };
+            return { .platform = Platform::PC, .version = LevelVersion::Tomb4, .raw_version = version };
         }
-        return { .platform = Platform::Unknown, .version = LevelVersion::Unknown };
+        return { .platform = Platform::Unknown, .version = LevelVersion::Unknown, .raw_version = version };
     }
 }
